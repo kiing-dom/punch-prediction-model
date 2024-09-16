14ca4c184bb374dbeae37543553f6e6506b60e01
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-09-16 20:52:41 +0100

    pose estimator is working

--- pose_estimator.py
+++ pose_estimator.py
@@ -5,16 +5,34 @@ mp_pose = mp.solutions.pose
 pose = mp_pose.Pose()
 mp_drawing = mp.solutions.drawing_utils
 
+# Function to process video frames
 def process_frame(frame):
-    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    
+    #process the frame and get the pose landmarks
     results = pose.process(rgb_frame)
     
-    if results.pose.landmarks:
+    if results.pose_landmarks:
         mp_drawing.draw_landmarks(
             frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS
         )
     return frame
 
-# TODO: Test above code ^^^
-# Capture video from camera or file input
+    # Capture video from camera or file input
+
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    success, frame = cap.read()
+    if not success:
+        break
     
+    annotated_frame = process_frame(frame)
+    
+    cv2.imshow('Pose Estimation', annotated_frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
